8ea5f0d9a34ba459bd5a2208e52d876e32afd303
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-08 19:12:18 +0300

    Update hi_shalom.py

--- hi_shalom.py
+++ hi_shalom.py
@@ -1,11 +1,12 @@
-"""
-This program reads a sentence and returns a list of all the words length.
-"""
+def words_length(string) ->list :
+    """
+    This function reads a sentence and returns a list of all the words length.
+    @param string: a sentence
+    @return: list of words length
+    """
+    return [len(word) for word in string.split()]
 
-def words_length(string):
-    words = string.split()
-    words_len = [len(word) for word in words]
-    return words_len
 
-
-print(words_length("Toto, I've a feeling we're not in Kansas anymore"))
+if __name__ == "__main__":
+    print(words_length("Toto, I've a feeling we're not in Kansas anymore"))
+    
