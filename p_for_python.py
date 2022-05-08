1cfca4f90a1eb9ee3dcc9c5b959013e40adcc23b
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-08 18:42:07 +0300

    Update p_for_python.py

--- p_for_python.py
+++ p_for_python.py
@@ -1,4 +1,4 @@
-def get_num():
+def get_num() -> list:
     """This function returns list of 2 lists"""
     list1 = [chr(num) for num in range(ord('A'), ord('['))]
     list2 = [chr(num) for num in range(ord('a'), ord('{'))]
