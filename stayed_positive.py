f515e6f191fb7435e5eab8a4e5563ad12ba5a2cd
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-08 19:09:54 +0300

    Update stayed_positive.py

--- stayed_positive.py
+++ stayed_positive.py
@@ -1,18 +1,16 @@
-"""
-This program receives list of numbers from the user, then returns only the positive numbers.
-"""
-
-
 def get_positive_numbers():
+    """
+    This function receives list of numbers from the user, then returns only the positive numbers.
+    @return: tuple of positive numbers
+    """
     numbers_list = input("enter list of numbers suppurated with ',' : ")
     user_list = numbers_list.split(',')
 
-    for i in range(len(user_list)):
-        # convert each item to int type
-        user_list[i] = int(user_list[i])
+    # convert each item to int type
+    [user_list[i] = int(user_list[i]) for i in range(len(user_list))]
 
-    positive_numbers = filter(lambda n: n > 0, user_list)
-    return tuple(positive_numbers)
+    return tuple(filter(lambda n: n > 0, user_list))
 
 
-print(get_positive_numbers())
+if __name__ == "__main__":
+    print(get_positive_numbers())
