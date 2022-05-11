729235eb00a136b44a19d1adbd13a2cc57e5cc82
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-11 13:14:43 +0300

    Update p_for_python.py

--- p_for_python.py
+++ p_for_python.py
@@ -2,7 +2,7 @@ def get_num() -> list:
     """This function returns list of 2 lists"""
     list1 = [chr(num) for num in range(ord('A'), ord('['))]
     list2 = [chr(num) for num in range(ord('a'), ord('{'))]
-    return list1, list2
+    return list1 + list2
 
 if __name__ == "__main__":
     print(get_num())
